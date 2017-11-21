26012d6287eaff2d558c8bf0a19a287d900af058
delafu <delafu@gmail.com>
2017-11-21 23:02:04 +0100

    debugging

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -38,7 +38,13 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 		particle.weight = 1;
 		particles.push_back(particle);
 		weights.push_back(1);
-		cout << particle.weight;
+		cout << "Init x: "
+		cout << particle.x;
+		cout << "\n";
+		cout << "Init y: "
+		cout << particle.y;
+		cout << "\n";
+		
 	}
 	is_initialized = true;
 }
@@ -71,7 +77,12 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 		particle.x=dist_x(gen);
 		particle.y=dist_y(gen);
 		particle.theta=dist_theta(gen);
-		
+		cout << "Pred x: "
+		cout << particle.x;
+		cout << "\n";
+		cout << "Pred y: "
+		cout << particle.y;
+		cout << "\n";		
 	}
 
 
